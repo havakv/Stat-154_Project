3c15236aa68821b42c97652a41868abc28b30eae
Haavard Kvamme <haavard.kvamme@gmail.com>
2014-04-18 11:23:05 -0700

    Added CV in test.R

--- methods/ldaQda/lda.R
+++ methods/ldaQda/lda.R
@@ -1,24 +1,8 @@
-# Script for lda and qda classifiers
+# Function for testing LDA with optimal predictor selection
 library(MASS)
 source("../test.R")
 source("../optRF/optRF.R")
 
-#-------------------------------------------------
-#path <- paste(findPath(), "train.csv", sep = '')
-#data <- read.csv(path, header = FALSE)
-#y <-  as.factor(data[,1])
-#X <- data[,-1]
-
-## LDA with CV
-#set.seed(0)
-#ldaCV <- lda(X, y, CV = TRUE)
-#sum(ldaCV$class != y)/length(y)
-#ldaFit <- lda(X, y, CV = FALSE)
-#ldaPred <- predict(ldaFit, X)
-#sum(ldaPred$class != y)/length(y)
-#-------------------------------------------------
-
-# Function for testing LDA with optimal predictor selection
 bestPred.lda2 <- function(X, y, ...){
   # Getting best predictors by sorting with optRF and choosing nr of predictors.
   error.lda2 <- function(nrPred, X, y, sortPred, seed, ...){
@@ -26,10 +10,9 @@ bestPred.lda2 <- function(X, y, ...){
     nrPred <- round(nrPred)
     bestPred <- sortPred[1:nrPred]
     set.seed(seed)
-    rat <- getRates(lda2, 1, 1/3, X[,bestPred], y, ...)
+    rat <- getRates(lda2, 1, 3, X[,bestPred], y, parallel = FALSE, 
+		    opt = FALSE, ...)
     errRate <- aveRates(rat)$tot[1]
-    #cat(errRate, "\n")
-    #cat(nrPred, "\n")
     return(errRate)
   }
   sortPred <- sortPred.optRF(X, y)
@@ -38,17 +21,15 @@ bestPred.lda2 <- function(X, y, ...){
 	       sortPred = sortPred, seed = seed, ...,
 	       method = "Brent", lower = 1, upper = length(sortPred),
 	       control = list(reltol = 1))
-  cat("Nr pred ", floor(opt$par), "\n")
-  cat("Val ", opt$value, "\n")
   return(sortPred[1:floor(opt$par)])
 }
 
-lda2 <- function(X, y, opt = FALSE, ...){
+lda2 <- function(X, y, opt = TRUE, ...){
   # Function for running LDA whith variable selection (opt = TRUE)
-  if (opt) bestPred <- bestPred.lda2(X, y, ...)
+  if (opt) {
+    bestPred <- bestPred.lda2(X, y, ...)
+  }
   else bestPred <- 1:ncol(X)
-  #cat("length(bestPred) ", length(bestPred), "\n")
-  #bestPred <- NULL
   obj <- lda(X[,bestPred], y, CV = FALSE, ...)
   obj$bestPred <- bestPred
   class(obj) <- c("lda2", class(obj))
@@ -60,36 +41,9 @@ predict.lda2 <- function(obj, testX){
   return(pred$class)
 }
 
-set.seed(0)
-#test1 <- test(lda2, nr = 24, parallel = TRUE, pushSplit = TRUE, nCores = 6)
-test1 <- test(lda2, nr = 16, parallel = TRUE, opt = TRUE)
-test1
+#set.seed(0)
+##options(error = recover)
+#test1 <- test(lda2, nr = 1, cv.fold = 4, parallel = TRUE)
+#test1
 
 
-#-------------------------------------------------
-## QDA with CV
-## We need < 100 predictors to be able to run qda
-#qdaCV <- qda(X[,101:200], y, CV = TRUE)
-#sum(is.na(qdaCV$class))
-#sum(qdaCV$class != y, na.rm = TRUE)/length(y)
-#
-##-------------------------------------------------
-#
-## Function for testing QDA with optimal predictor selection
-#qda2 <- function(X, y, ...){
-#  # Need a function to pick optimal predictors
-#  # Nr of predictors must be smaller than the smallest nr of one factor in the groups
-#  # Max amount of predictors allowed in qda
-#  maxNrPred <- min(as.integer(table(y)))-1
-#
-#  obj <- qda(X[,101:130], y, CV = FALSE, ...)
-#  class(obj) <- c("qda2", class(obj))
-#  return(obj)
-#}
-#predict.qda2 <- function(obj, testX){
-#  class(obj) <- "qda"
-#  pred <- predict(obj, testX[,101:130])
-#  return(pred$class)
-#}
-#
-#test(qda2, nr = 32, parallel = TRUE)
--- methods/test.R
+++ methods/test.R
@@ -2,6 +2,7 @@
 require(parallel) # one of the core R packages
 require(doParallel)
 library(foreach)
+library(cvTools)
 
 findPath <- function(){
   # Finds the path from your working directory to the "methods" folder
@@ -11,27 +12,7 @@ findPath <- function(){
   path <- paste(rep("../", n-nr), collapse = '')
 }
 
-splitData <- function( X, y, testProp = 1/3){
-  # Split the data in train and test set.
-  # testProp is the proportion of the dataset that will be used for testing.
-  # so (1 - testProp) is the proportion for training.
-  n <- length(y)
-  if (testProp >= 1 | testProp <= 0)
-    stop("testProp not in (0,1)")
-  sp <- ceiling(n*(1-testProp))
-  if (sp == 1)
-    stop("No data in train")
-  shuffled.order <- sample(n)
-  train <- shuffled.order[1:sp] #about 2/3 of the data
-  test <- shuffled.order[(sp+1):n]
-  trainX <- X[train,]
-  trainy <- y[train]
-  testX <- X[test,]
-  testy <- y[test]
-  return(list(testX = testX, testy = testy, trainX = trainX, trainy = trainy))
-}
-
-missclass <- function(pred, y, level){
+misclass <- function(pred, y, level){
   # See errorRates
   ix <- which(y == level)
   missy <- sum(pred[ix] != y[ix])/length(ix)
@@ -50,33 +31,61 @@ errorRates <- function(pred, testy){
   rownames(ErrRates) <- 1:8
   colnames(ErrRates) <- c("should", "shouldn't")
   for (i in 1:8)
-    ErrRates[i,] <- missclass(pred, testy, i)
+    ErrRates[i,] <- misclass(pred, testy, i)
   tot <- sum(pred != testy)/length(testy)
   return(list(tot = tot, indiv = ErrRates))
 }
 
-getRatesInPar <- function(method, nr = 1, testProp = 1/3, nCores, 
-			  X, y, ...){
-  # Parallel version of getRates
-  # Returns 17 x nr matrix. Each row represent a rate.
-  # The reason for the format is efficiency in aveRates.
-  # Remember to set: export OMP_NUM_THREADS=1 on machine
-  #require(parallel) # one of the core R packages
-  #require(doParallel)
-  #library(foreach)
+crossVal <- function(method, cv.fold = 3, X, y, ...){
+  n <- length(y)
+  if (cv.fold > n)
+    stop("cv.fold > length(y)")
+  shuffled.order <- sample(n)
+  groupSizes <- as.numeric(table(cvFolds(n, cv.fold)$which))
+  ixGroup <- c(0, cumsum(groupSizes))
+
+  Rates <- matrix(rep(NA, cv.fold*17), 17, cv.fold)
+  for (i in 1:cv.fold){
+    ixTest <- (1+ixGroup[i]):ixGroup[i+1]
+    test   <- shuffled.order[ixTest]
+    train  <- shuffled.order[-ixTest]
+    trainX <- X[train,]
+    trainy <- y[train]
+    testX  <- X[test,]
+    testy  <- y[test]
+    obj <- method(trainX, trainy, ...)
+    pred <- predict(obj, testX)
+    rat <- errorRates(pred, testy)
+    Rates[,i] <- c(rat$tot, rat$indiv[,1], rat$indiv[,2])
+  }
+  return(Rates)
+}
+
+crossValInPar <- function(method, cv.fold = 3, X, y, nCores = 2, ...){
   if(as.numeric(system("echo $OMP_NUM_THREADS", intern = TRUE))	!= 1)
     stop("OMP_NUM_THREADS is not 1")
+  n <- length(y)
+  if (cv.fold > n)
+    stop("cv.fold > length(y)")
+  shuffled.order <- sample(n)
+  groupSizes <- as.numeric(table(cvFolds(n, cv.fold)$which))
+  ixGroup <- c(0, cumsum(groupSizes))
 
   registerDoParallel(nCores)
-  seeds <- round(runif(nr, 0, .Machine$integer.max))
-  Rates <- matrix(rep(NA, nr*17), 17, nr)
-  Rates <- foreach(i = 1:nr, .combine = cbind) %dopar% {
+  Rates <- matrix(rep(NA, cv.fold*17), 17, cv.fold)
+
+  Rates <- foreach(i = 1:cv.fold, .combine = cbind) %dopar% {
     cat("Starting ", i, "\n", sep = '')
-    set.seed(seeds[i])
-    Split <- splitData(X, y, testProp)
-    obj <- method(Split$trainX, Split$trainy, ...)
-    pred <- predict(obj, Split$testX)
-    rat <- errorRates(pred, Split$testy)
+    ixTest <- (1+ixGroup[i]):ixGroup[i+1]
+    test   <- shuffled.order[ixTest]
+    train  <- shuffled.order[-ixTest]
+    trainX <- X[train,]
+    trainy <- y[train]
+    testX  <- X[test,]
+    testy  <- y[test]
+    obj <- method(trainX, trainy, ...)
+    pred <- predict(obj, testX)
+    rat <- errorRates(pred, testy)
     rates <- c(rat$tot, rat$indiv[,1], rat$indiv[,2])
     cat("Ending ", i, "\n", sep = '')
     rates
@@ -84,18 +93,41 @@ getRatesInPar <- function(method, nr = 1, testProp = 1/3, nCores,
   return(Rates)
 }
 
-getRates <- function(method, nr = 1, testProp = 1/3, X, y, ...){
+getRatesInPar <- function(method, nr = 1, cv.fold = 3, nCores, 
+			  X, y, ...){
+  # Parallel version of getRates
   # Returns 17 x nr matrix. Each row represent a rate.
   # The reason for the format is efficiency in aveRates.
+  # Remember to set: export OMP_NUM_THREADS=1 on machine
+  if(as.numeric(system("echo $OMP_NUM_THREADS", intern = TRUE))	!= 1)
+    stop("OMP_NUM_THREADS is not 1")
+
+  registerDoParallel(nCores)
   seeds <- round(runif(nr, 0, .Machine$integer.max))
-  Rates <- matrix(rep(NA, nr*17), 17, nr)
+  Rates <- matrix(rep(NA, nr*cv.fold*17), 17, nr*cv.fold)
+  Rates <- foreach(i = 1:nr, .combine = cbind) %dopar% {
+    cat("Starting ", i, "\n", sep = '')
+    set.seed(seeds[i])
+    rates <- crossVal(method, cv.fold, X, y, ...)
+    cat("Ending ", i, "\n", sep = '')
+    rates
+  }
+  return(Rates)
+}
+
+getRates <- function(method, nr = 1, cv.fold = 3, X, y, parallel = FALSE, 
+		     nCores = 2, ...){
+  # Returns 17 x cv.fold*nr matrix. Each row represent a rate.
+  # The reason for the format is efficiency in aveRates.
+  seeds <- round(runif(nr, 0, .Machine$integer.max))
+  Rates <- matrix(rep(NA, nr*cv.fold*17), 17, nr*cv.fold)
   for (i in 1:nr){
     set.seed(seeds[i])
-    Split <- splitData(X, y, testProp)
-    obj <- method(Split$trainX, Split$trainy, ...)
-    pred <- predict(obj, Split$testX)
-    rat <- errorRates(pred, Split$testy)
-    Rates[,i] <- c(rat$tot, rat$indiv[,1], rat$indiv[,2])
+    pos <- (1+(i-1)*cv.fold):(i*cv.fold)
+    if (parallel)
+      Rates[,pos] <- crossValInPar(method, cv.fold, X, y, nCores, ...)
+    else
+      Rates[,pos] <- crossVal(method, cv.fold, X, y, ...)
   }
   return(Rates)
 }
@@ -116,7 +148,7 @@ aveRates <- function(Rates){
   return(list(tot = tot, indiv = indiv))
 }
 
-test <- function(method, nr = 1, testProp = 1/3, path = findPath(),
+test <- function(method, nr = 1, cv.fold = 3, path = findPath(),
 		parallel = FALSE, nCores = 2, ...){
   # Function to run test on method
   # Get data
@@ -124,13 +156,8 @@ test <- function(method, nr = 1, testProp = 1/3, path = findPath(),
   data <- read.csv(path, header = FALSE)
   y <-  as.factor(data[,1])
   X <- data[,-1]
-
   # Run method nr times and get error rates
-  if (parallel & nr != 1)
-    Rates <- getRatesInPar(method, nr, testProp, nCores, X, y, ...)
-  else
-    Rates <- getRates(method, nr, testProp, X, y, ...)
-
+  Rates <- getRates(method, nr, cv.fold, X, y, parallel, nCores, ...)
   return(aveRates(Rates))
 }
 
@@ -138,6 +165,7 @@ test <- function(method, nr = 1, testProp = 1/3, path = findPath(),
 # Put this in you function file (see optRF.R)
 #library(randomForest)
 #set.seed(0)
-#test(randomForest, nr = 4)
+#test(randomForest, nr = 1, cv.fold = 6)
 #set.seed(0)
-#test(randomForest, nr = 4, parallel = TRUE)
+#test(randomForest, nr = 2, cv.fold = 3, parallel = TRUE)
+
